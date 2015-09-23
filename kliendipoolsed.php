ab6c4d79b3f19f4a0c2d053a052b03a467cfb0ca
Rajur <rajur.hint@gmail.com>
2015-09-23 12:11:44 +0100

    kassipilt koerapildiks

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,10 +3,22 @@
         <head>
             <link rel="stylesheet" type="text/css" href="theme.css">
             <title></title>
-            <script>
+            <script language="javascript">
                 function myFunction() {
                     alert("Tere maailm!");
                 }
+
+            </script>
+                <script>
+                function dogFunction() {
+
+                    if (document.getElementById("pilt").src = "img/cat.jpg")
+                    {
+                        document.getElementById("pilt").src = "img/cute.dog.jpg";
+                    }
+
+                }
+
 
             </script>
         </head>
@@ -14,9 +26,12 @@
         <button onclick="myFunction()">Tere maailm!</button>
         <br>
             <a href="http://www.khk.ee" onclick="alert('Tere maailm!')">Tere maailm!</a>
+        <br>
             <a href onclick="alert('jääme siia')">jääme siia</a>
+        <br>
 
-            <img src="img/cat.jpg" onclick="">
+            <!--pilt-->
+        <img alt="" src="img/cat.jpg" style="height: 185px; width: 198px" id= "pilt" onclick="dogFunction()" />
 
         </body>
     </html>
