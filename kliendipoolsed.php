1574e6acdfe7fa7a7237a3787a816d14d20fd800
Rajur <rajur.hint@gmail.com>
2015-09-23 09:16:15 +0100

    kassipilt lisatud

---
+++ kliendipoolsed.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+    <html>
+        <head>
+            <title></title>
+            <script>
+                function myFunction() {
+                    alert("Tere maailm!");
+                }
+
+            </script>
+        </head>
+        <body>
+        <button onclick="myFunction()">Tere maailm!</button>
+        <br>
+            <a href="http://www.khk.ee" onclick="alert('Tere maailm!')">Tere maailm!</a>
+            <br>
+
+
+            <img src="img/cat.jpg">
+
+        </body>
+    </html>
