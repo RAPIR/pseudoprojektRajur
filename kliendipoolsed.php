f32ffd35845684bc20ebcfebb63955bc47e5224e
Rajur <rajur.hint@gmail.com>
2015-10-17 17:38:14 +0100

    fixes

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,37 +1,39 @@
 <!DOCTYPE>
-    <html>
-        <head>
-            <link rel="stylesheet" type="text/css" href="theme.css">
-            <title></title>
-            <script language="javascript">
-                function myFunction() {
-                    alert("Tere maailm!");
-                }
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <title>Document</title>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 
-            </script>
-                <script>
-                function dogFunction() {
+</head>
+<body>
 
-                    if (document.getElementById("pilt").src = "img/cat.jpg")
-                    {
-                        document.getElementById("pilt").src = "img/cute.dog.jpg";
-                    }
+<!-- link--->
 
-                }
+<script language="javascript">
+    function myFunction() {
+        alert("Tere maailm!");
+    }
+</script>
 
+<!-- alert-->
 
-            </script>
-        </head>
-        <body>
-        <button onclick="myFunction()">Tere maailm!</button>
-        <br>
-            <a href="http://www.khk.ee" onclick="alert('Tere maailm!')">Tere maailm!</a>
-        <br>
-            <a href onclick="alert('jääme siia')">jääme siia</a>
-        <br>
+<button onclick="myFunction()">Tere maailm!</button>
+<br>
 
-            <!--pilt-->
-        <img alt="" src="img/cat.jpg" style="height: 185px; width: 198px" id= "pilt" onclick="dogFunction()" />
+<a href="http://www.khk.ee" onclick="alert('Tere maailm!')">Tere maailm!</a>
+<br>
+<a href onclick="alert('jääme siia')">jääme siia</a>
+<br>
 
-        </body>
-    </html>
+<!--Kassipilt koerapildiks jQuery-->
+<img alt="" src="img/cat.jpg" style="height: 185px; width: 198px" id= "pilt" />
+
+<script>
+    $("img[src='img/cat.jpg']").click(function () {
+        $(this).attr("src","img/cute.dog.jpg")
+    })
+</script>
+</body>
+</html>
