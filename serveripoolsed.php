693ba4d44b7ef0842963c90aaff87b66746a66ea
Rajur <rajur.hint@gmail.com>
2015-11-02 12:45:50 +0000

    1429986-103542550 15. Boolean

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,10 +7,12 @@
 <body>
 <h6>
     <?php
-        $age = 26;
-        $first='Rajur';
-        $last='Hint';
-        echo "$first $last ($age)";
+    $eesnimi_algab_vokaaliga = false;
+    $age = 26;
+    $first='Rajur';
+    $last='Hint';
+    echo $first . " " . $last . " (" . $age . ")";
+
     ?>
 </h6>
 </body>
