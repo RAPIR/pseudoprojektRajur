b057c57d5483fee4a9586cc0abe62356c508e0ae
Rajur <rajur.hint@gmail.com>
2015-11-02 19:26:35 +0000

    1429986-103580766 19. Funktsioonid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,11 +7,29 @@
 <body>
 
 <?php
-    $isik = (object)array("first" => "Rajur", "last" => "Hint", "age" => 26);
-    $isik->sugu = "mees";
-    $eesnimi_algab_vokaaliga = false;
+
+$isik = (object)array("first" => "Rajur", "last" => "Hint", "age" => 26);
+$isik->sugu = "mees";
+function first_start($other)
+{
+    if (in_array($other[0], ["a", "e", "i", "o", "u", "õ", "ä", "ö", "ü"])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+;
+$eesnimi_algab_vokaaliga = first_start($isik->first);
 
 echo $isik->first . " " . $isik->last . " (" . $isik->age . ", " . $isik->sugu . ")";
+
+if ($eesnimi_algab_vokaaliga) {
+    echo "Nimi algab vokaaliga";
+}
+else{
+    echo   'Nimi ei alga vokaaliga.';
+};
+
 
 ?>
 
