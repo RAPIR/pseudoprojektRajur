f8fedafe785bfbf5d7823bc71a556d1c24e68e03
Rajur <rajur.hint@gmail.com>
2015-11-02 17:59:59 +0000

    1429986-103580708 18. Objektid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,25 +7,12 @@
 <body>
 
 <?php
-$eesnimi_algab_vokaaliga = false;
-$vocals=array('a','e','i','o','u','õ','ä','ö','ü');
-$isik=array('first'=>'Rajur ', 'last'=>'Hint ', 'age'=>'(26)'
-);
+    $isik = (object)array("first" => "Rajur", "last" => "Hint", "age" => 26);
+    $isik->sugu = "mees";
+    $eesnimi_algab_vokaaliga = false;
 
-foreach($isik as $key=>$val){
-    echo $val;
-};
-echo '<br><br>';
+echo $isik->first . " " . $isik->last . " (" . $isik->age . ", " . $isik->sugu . ")";
 
-for($i = 0; $i<=count(@explode(',',$vocals));$i++){
-    if(substr(strtoUpper($isik['first']),0,1) == $vocals[$i]){
-        echo  'Nimi algab vokaaliga.';
-    }
-
-    else{
-        echo   'Nimi ei alga vokaaliga.';
-    };
-};
 ?>
 
 </body>
