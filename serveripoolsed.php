3adeb6a320af2a45fc614e4081a459aa8eb79081
Rajur <rajur.hint@gmail.com>
2015-11-02 11:52:51 +0000

    1429986-103542512 14. Täisarvud

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,9 +7,10 @@
 <body>
 <h6>
     <?php
+        $age = 26;
         $first='Rajur';
         $last='Hint';
-        echo "$first $last";
+        echo "$first $last ($age)";
     ?>
 </h6>
 </body>
