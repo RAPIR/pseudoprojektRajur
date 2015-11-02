450188a9761e2c753c2ac15fefeda02b7d3c1017
Rajur <rajur.hint@gmail.com>
2015-11-02 20:10:09 +0000

    1429986-103580824 22. $_POST

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,13 +6,17 @@
 </head>
 <body>
 
+<form action="?" method="post">
+    <label for="kass">Sfinks:</label>
+    <input type="text" id="kass" name="kass">
+    <input type="submit" value="Submit">
+</form>
+
 <?php
 
-if (!empty($_GET['koer'])) {
-    echo " ".$_GET['koer']." ";
-}
-
-
+if(isset($_POST['kass'])) {
+        echo 'Kass oli: ' . $_POST['kass'] . '.';
+    }
 ?>
 
 </body>
